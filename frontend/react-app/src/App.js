983cb189d7ece3cf55101a33c411632e9871411f
Igor Soares <soares.igorh@gmail.com>
2023-01-15 16:43:20 -0300

    Criado o componente NavBar e separado o css dele

--- frontend/react-app/src/App.js
+++ frontend/react-app/src/App.js
@@ -1,11 +1,42 @@
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-      </header>
+    <div className="main-container">
+
+      <Navbar />
+      <section class="container">
+          <header class="main-header">
+              Consulta de Alunos
+          </header>
+          <div class="loader"></div>
+          <div class="content-page display-none">
+              <div class="padding-left-right-20">
+                  <div class="top-actions">
+                      <form id="formSearchStudent" class="form-search">
+                          <input type="text" name="searchInput" id="searchInput"/>
+                          <button>Pesquisar</button>
+                      </form>
+                      <a class="btn btn-dark" href="studentManager.html">Cadastrar Aluno</a>
+                  </div>
+                  <table id="studentList" class="table-list">
+                      <thead>
+                          <tr>
+                              <th>Registro Acadêmico</th>
+                              <th>Nome</th>
+                              <th>CPF</th>
+                              <th>Ações</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                          
+                      </tbody>
+                  </table>
+              </div>
+
+          </div>
+      </section>
     </div>
   );
 }
