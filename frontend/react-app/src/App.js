4b8cab6c92643d052982a8e8f94ee17b5da246ee
Igor Soares <soares.igorh@gmail.com>
2023-01-16 18:58:24 -0300

    Instalado react-router-dom | Configurado aplicação para trabalhar com rotas

--- frontend/react-app/src/App.js
+++ frontend/react-app/src/App.js
@@ -1,23 +1,29 @@
 import Navbar from './components/shared/Navbar';
 import StudentListPage from './components/pages/StudentsListPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="main-container">
+    <BrowserRouter>
+      <div className="main-container">
+        <Navbar />
+        <section className="container">
+            <header className="main-header">
+                Consulta de Alunos
+            </header>
+            <div className="content-page">
+              <Routes>
 
-      <Navbar />
-      <section className="container">
-          <header className="main-header">
-              Consulta de Alunos
-          </header>
-          <div className="content-page">
-            <StudentListPage />
-              
-
-          </div>
-      </section>
-    </div>
+                <Route path='/' element={ <StudentListPage /> } />
+                <Route path='/student-manager' element={ <h1>Student manager page</h1> }/>
+                
+                
+              </Routes>
+            </div>
+        </section>
+      </div>
+    </BrowserRouter>
   );
 }
 
