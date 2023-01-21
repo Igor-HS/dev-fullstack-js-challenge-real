51c8ebf0febec98d2b8b3f9cf6e99c0ac7fe8b74
Igor Soares <soares.igorh@gmail.com>
2023-01-20 22:16:17 -0300

    Alterado backgroundColor nos campos readonly para editar estudante

--- frontend/react-app/src/components/pages/StudentsManagerPage/index.jsx
+++ frontend/react-app/src/components/pages/StudentsManagerPage/index.jsx
@@ -8,8 +8,6 @@ const StudentManagerPage = () => {
 
     const {id} = useParams();
 
-    console.log(id);
-
     const [isRedirect, setIsRedirect] = useState(false);
     const [isLoading, updateIsLoading] = useState(false);
 
@@ -19,11 +17,13 @@ const StudentManagerPage = () => {
     const [fieldCpf, updateFieldCpf] = useState({
         value:"",
         isReadOnly: false,
+        isEnabled: "white"
     })
 
     const [fieldRa, updateFieldRa] = useState({
         value: "",
-        isReadOnly: false
+        isReadOnly: false,
+        isEnabled: "white"
     })
 
 
@@ -40,10 +40,12 @@ const StudentManagerPage = () => {
             updateFieldCpf({
                 isReadOnly: true,
                 value: data.cpf,
+                isEnabled: "#dddddd"
             });
             updateFieldRa({
                 isReadOnly: true,
                 value: data.ra,
+                isEnabled: "#dddddd"
             })
 
             updateIsLoading(false);
@@ -109,59 +111,62 @@ const StudentManagerPage = () => {
 
     return (
         <>
-        <header className="main-header">
-            <span>Cadastrar/Editar Aluno</span>
-        </header>
-        
-        <div className="content-page padding-left-right-20"> 
-            <form id="studentForm" className="form" action="" method="post"
-            onSubmit={onSubmitForm}>
-                <div className="form-group">
-                    <label htmlFor="name">Nome</label>
-                    <input required type="text" name="name" id="name" value={name}
-                    placeholder="Digite o seu nome"
-                    onChange={(event)=>{
-                        updateName(event.target.value);
-                    }}/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input required type="email" name="email" id="email" value={email}
-                    placeholder="Digite o seu email"
-                    onChange={(event)=>{
-                        updateEmail(event.target.value);
-                    }}/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="ra">RA</label>
-                    <input required type="number" name="ra" id="ra" value={fieldRa.value}
-                    readOnly={fieldRa.isReadOnly}
-                    placeholder="Digite o seu ra"
-                    onChange={(event)=>{
-                        updateFieldRa({
-                            ...fieldRa,
-                            value: event.target.value});
-                    }}/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="cpf">CPF</label>
-                    <input required type="number" name="cpf" id="cpf" value ={fieldCpf.value}
-                    readOnly={fieldCpf.isReadOnly}
-                    placeholder="Digite o seu cpf"
-                    onChange={(event)=>{
-                        updateFieldCpf({
-                            ...fieldCpf,
-                            value: event.target.value});
-                    }}/>
-                </div>
-                <div className="actions">
-                    <Link to="/" className="btn btn-warning margin-right-10">
-                        Cancelar
-                    </Link>
-                    <button className="btn">Salvar</button>
-                </div>
-            </form>         
-        </div>
+            <header className="main-header">
+                <span>Cadastrar/Editar Aluno</span>
+            </header>
+            
+            
+            <div className="content-page padding-left-right-20"> 
+                <form id="studentForm" className="form" action="" method="post"
+                onSubmit={onSubmitForm}>
+                    <div className="form-group">
+                        <label htmlFor="name">Nome</label>
+                        <input required type="text" name="name" id="name" value={name}
+                        placeholder="Digite o seu nome"
+                        onChange={(event)=>{
+                            updateName(event.target.value);
+                        }}/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input required type="email" name="email" id="email" value={email}
+                        placeholder="Digite o seu email"
+                        onChange={(event)=>{
+                            updateEmail(event.target.value);
+                        }}/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="ra">RA</label>
+                        <input required type="number" name="ra" id="ra" value={fieldRa.value}
+                        readOnly={fieldRa.isReadOnly}
+                        placeholder="Digite o seu ra"
+                        style={{backgroundColor: fieldCpf.isEnabled}}
+                        onChange={(event)=>{
+                            updateFieldRa({
+                                ...fieldRa,
+                                value: event.target.value});
+                        }}/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="cpf">CPF</label>
+                        <input required type="number" name="cpf" id="cpf" value ={fieldCpf.value}
+                        readOnly={fieldCpf.isReadOnly}
+                        placeholder="Digite o seu cpf"
+                        style={{backgroundColor: fieldCpf.isEnabled}}
+                        onChange={(event)=>{
+                            updateFieldCpf({
+                                ...fieldCpf,
+                                value: event.target.value});
+                        }}/>
+                    </div>
+                    <div className="actions">
+                        <Link to="/" className="btn btn-warning margin-right-10">
+                            Cancelar
+                        </Link>
+                        <button className="btn">Salvar</button>
+                    </div>
+                </form>
+            </div>         
         </>
     )
     
